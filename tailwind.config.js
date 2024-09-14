1e7b3c14885beab03eca39d722f97945c1e12ba4
Fareedah <pharydah.a@gmail.com>
2024-09-14 21:09:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'royal-blue': '#4169E1', //added the royal blue color
+      },
+    },
+  },
+  plugins: [],
+}
+
